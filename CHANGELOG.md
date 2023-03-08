3d5338cc79731c57c595fca1a4a714021b3b1773
Robert Forkel <xrotwang@googlemail.com>
2023-03-08 14:12:36 +0100

    bump version for development

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changes
 
+## [Unreleased]
+
 
 ## [v1.0.0] - 2023-03-08
 
--- src/commonnexus/__init__.py
+++ src/commonnexus/__init__.py
@@ -1,4 +1,4 @@
 from .nexus import Nexus, Config  # noqa: F401
 from commonnexus.blocks import Block  # noqa: F401
 
-__version__ = '1.0.0'
+__version__ = '1.0.1.dev0'
