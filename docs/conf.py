ad4ca26e544524c92717ea2dd1422f3ec219eabf
Robert Forkel <xrotwang@googlemail.com>
2023-03-08 14:10:27 +0100

    release 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'commonnexus'
 copyright = '2023, Robert Forkel'
 author = 'Robert Forkel'
-release = '0.1.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
