898ab68db93607463ed6622451307471a3865d78
Robert Forkel <xrotwang@googlemail.com>
2026-02-24 15:23:34 +0100

    bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'commonnexus'
 copyright = '2026, Robert Forkel'
 author = 'Robert Forkel'
-release = '2.0.0'
+release = '2.0.1.dev0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
